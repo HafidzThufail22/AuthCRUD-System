5069fc55330e715c8badc11d4318667f17dbe1e2
HafidzThufail <163546242+HafidzThufail22@users.noreply.github.com>
2026-01-06 23:00:25 +0700

    feat(backend): update migrasi tabel kotas

--- backend/database/migrations/2025_12_27_154033_create_kotas_table.php
+++ backend/database/migrations/2025_12_27_154033_create_kotas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('kotas', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kota');
-            $table->integer('propinsi_id'); // Sesuai PDF [cite: 166]
+            $table->integer('propinsi_id'); 
             $table->timestamps();
         });
     }
